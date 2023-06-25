fa0a981841bb833ffb48f522093d5e40cf1bf5cb
SgtFreddy713 <captesxel@gmail.com>
2023-06-25 17:41:37 -0300

    Button added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let age = Math.floor(Math.random() * 100);
+/*let age = Math.floor(Math.random() * 100);
 
 if (age < 21) {
     console.log("You can not enter the club!");
 } else {
     console.log("Welcome")
-}
+}*/
 
 let firstCard = Math.floor(Math.random() * 10) + 2;
 let secondCard = Math.floor(Math.random() * 10) + 2;
@@ -13,14 +13,15 @@ let hasBlackJack = false;
 let isAlive = true
 let message = ""
 
-if (sum < 21) {
-    message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game! 😭";
-    isAlive = false;
+function startGame() {
+    if (sum < 21) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    }
+    console.log(message)
 }
-
-console.log(message)
