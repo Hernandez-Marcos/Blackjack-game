fcf8c8ecce1d02ea625f5f684ba2a6f5eda6939b
SgtFreddy713 <captesxel@gmail.com>
2023-06-26 00:26:56 -0300

    functions added

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ if (age < 21) {
     console.log("Welcome")
 }*/
 
-let firstCard = Math.floor(Math.random() * 10) + 2;
-let secondCard = Math.floor(Math.random() * 10) + 2;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById("sum-el")
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1;
+    console.log(randomNumber)
+    return randomNumber > 10 ? 10 : randomNumber === 1 ? 11 : randomNumber;
+}
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
     if (sum < 21) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
@@ -25,6 +37,19 @@ function startGame() {
         message = "You're out of the game!";
         isAlive = false;
     };
+
+    cardsEl.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
     messageEl.textContent = message;
     sumEl.textContent = "Sum: " + sum;
+}
+
+function newCard() {
+    card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    console.log(cards)
+    renderGame();
 }
