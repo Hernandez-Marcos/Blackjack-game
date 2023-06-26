7919dc68f5b6ce729dd6c5ebf2f7e195688557b0
SgtFreddy713 <captesxel@gmail.com>
2023-06-25 22:24:03 -0300

    Added sum

--- index.js
+++ index.js
@@ -10,18 +10,21 @@ let firstCard = Math.floor(Math.random() * 10) + 2;
 let secondCard = Math.floor(Math.random() * 10) + 2;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
-let isAlive = true
-let message = ""
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el")
 
 function startGame() {
     if (sum < 21) {
-        message = "Do you want to draw a new card? 🙂";
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳";
+        message = "Wohoo! You've got Blackjack!";
         hasBlackJack = true;
     } else {
-        message = "You're out of the game! 😭";
+        message = "You're out of the game!";
         isAlive = false;
-    }
-    console.log(message)
+    };
+    messageEl.textContent = message;
+    sumEl.textContent = "Sum: " + sum;
 }
